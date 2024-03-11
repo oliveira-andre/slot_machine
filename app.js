0aa8886424ca77d32e792dc3177ef98f2a9243c5
Andre Oliveira <andreoliveirar2d2@gmail.com>
2024-03-10 23:05:35 -0300

    feat: including slot machine background, screen and lever

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
   const items = [
     "𝟳",
-    "❌",
-    "🍓",
-    "🍋",
-    "🍉",
     "🍒",
     "💵",
-    "🍊",
     "🍎"
   ];
 
@@ -19,6 +14,8 @@ document.addEventListener("DOMContentLoaded", function() {
   spinner.addEventListener("click", spin);
 
   async function spin() {
+    document.querySelector('h2').textContent = '🎰 GOOD LUCK! 🎰';
+    pullLeverAnimation(spinner);
     init();
     init(false, 1, 2);
 
@@ -32,10 +29,36 @@ document.addEventListener("DOMContentLoaded", function() {
     };
   };
 
+  function pullLeverAnimation(lever) {
+    playSound('lever');
+    if (screen.width < 1000) {
+      lever.style.animation = 'pullLeverMobile 0.6s ease';
+    } else {
+      lever.style.animation = 'pullLever 0.6s ease';
+    }
+        
+    // xReset animation for subsequent clicks
+    lever.addEventListener('animationend', () => {
+        lever.style.animation = '';
+    });
+  }
+
+  function playSound(sound) {
+    switch (sound) {
+        case 'lever':
+          var audio = document.getElementById('lever_sound');
+          audio.play();
+          break;
+        case 'selection':
+          var audio = document.getElementById('selection_sound');
+          audio.play();
+          break;          
+    }
+  }
+
   function init(firstInit = true, groups = 1, duration = 1) {
     for (const door of doors) {
       if (firstInit) {
-        document.querySelector('h2').textContent = '🎰 GOOD LUCK! 🎰';
         door.dataset.spinned = "0";
       } else if (door.dataset.spinned === "1") {
         return;
@@ -64,12 +87,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }, { once: true });
 
         boxesClone.addEventListener("transitionend", function() {
+          playSound('selection');
           this.querySelectorAll(".box").forEach((box, index) => {
             box.style.filter = "blur(0)";
 
             if (index > 0) this.removeChild(box);
           });
 
+
+          playSound('selection');
           checkResult(this);
         }, { once: true });
       };
@@ -113,9 +139,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const win = result.every((val, i, arr) => val === arr[0]);
 
     if (win) {
-      document.querySelector('h2').textContent = 'You win!';
+      document.querySelector('h2').textContent = 'YOU WIN!';
     } else {
-      document.querySelector('h2').textContent = 'You lose!';
+      document.querySelector('h2').textContent = 'YOU LOOSE!';
     }
   };
 
